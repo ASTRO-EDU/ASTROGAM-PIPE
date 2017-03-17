2092327aa4d0c5c9601f17d6be289332b3cf0db0
Giovanni Giannella <giovanni.giannella@studio.unibo.it>
2017-03-17 19:08:07 +0100

    Main program. It analyzes the fits files generated by BoGEMMS.

--- eASTROGAM_ANALYSISv1_file_remote.py
+++ eASTROGAM_ANALYSISv1_file_remote.py
@@ -145,6 +145,7 @@ while ifile <= n_fits:
 
 			dhseASTROGAM.build_L0()
 
+#			dhseASTROGAM.ene_strip_plan_y()
 
 			# Level 0 = energy summed
 			# Level 0 = the events are sorted in tray, and Y before X within the same tray
@@ -189,6 +190,8 @@ while ifile <= n_fits:
 			print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 
 			dhseASTROGAM.merging()			
+
+			dhseASTROGAM.ene_cluster_plan_y()
 
 			# Level 0.5 = energy summed, MIP threshold applied, strip position used
 
